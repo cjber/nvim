edb97146886466d28db3fa1eaa1dd24eeb7beab9
cjber <cjberragan@gmail.com>
2023-09-09 11:19:10 +0100

    change to <CMD>

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,6 +1,6 @@
 -- keymaps
-vim.api.nvim_set_keymap("n", "<CR>", ":REPLSendLine<CR>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>al", ":REPLStart ipython<CR>:wincmd p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>a;", ":REPLToCurLine<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>ad", ":REPLHideOrFocus<CR>:wincmd p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<CR>", ":REPLSendVisual<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<CR>", "<CMD>REPLSendLine<CR>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>al", "<CMD>REPLStart ipython<CR>:wincmd p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>a;", "<CMD>REPLToCurLine<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>ad", "<CMD>REPLHideOrFocus<CR>:wincmd p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<CR>", "<CMD>REPLSendVisual<CR>", { noremap = true, silent = true })
