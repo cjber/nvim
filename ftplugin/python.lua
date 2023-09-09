aa5e58fa72ab538dbca6acc1860dea5b1c9a75ef
cjber <cjberragan@gmail.com>
2023-09-09 10:53:01 +0100

    add missing <CR>

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -3,4 +3,4 @@ vim.api.nvim_set_keymap("n", "<CR>", ":REPLSendLine<CR>j", { noremap = true, sil
 vim.api.nvim_set_keymap("n", "<Leader>al", ":REPLStart ipython<CR>:wincmd p<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>a;", ":REPLToCurLine<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>ad", ":REPLHideOrFocus<CR>:wincmd p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<CR>", ":REPLSendVisual", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<CR>", ":REPLSendVisual<CR>", { noremap = true, silent = true })
