0b2952ba1508f6a5e3836a1cf1054d8e2f01963e
cjber <cjberragan@gmail.com>
2024-02-22 21:57:35 +0000

    linting

--- chadrc.lua
+++ chadrc.lua
@@ -2,43 +2,43 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require("custom.highlights")
+local highlights = require "custom.highlights"
 
 M.ui = {
-	theme = "catppuccin",
-	lsp_semantic_tokens = true,
+  theme = "catppuccin",
+  lsp_semantic_tokens = true,
 
-	hl_override = highlights.override,
-	hl_add = highlights.add,
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 
-	cmp = {
-		icons = true,
-		lspkind_text = true,
-		style = "flat_dark",
-		selected_item_bg = "simple",
-	},
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "flat_dark",
+    selected_item_bg = "simple",
+  },
 
-	telescope = { style = "borderless" },
+  telescope = { style = "borderless" },
 
-	statusline = {
-		theme = "minimal",
-		separator_style = "block",
-		-- modules arg here is the default table of modules
-		overriden_modules = function(modules)
-			table.remove(modules, 1)
-		end,
-	},
-	tabufline = {
-		show_numbers = false,
-		enabled = true,
-		lazyload = true,
-		overriden_modules = function(modules)
-			table.remove(modules, 4)
-		end,
-	},
+  statusline = {
+    theme = "minimal",
+    separator_style = "block",
+    -- modules arg here is the default table of modules
+    overriden_modules = function(modules)
+      table.remove(modules, 1)
+    end,
+  },
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = function(modules)
+      table.remove(modules, 4)
+    end,
+  },
 
-	nvdash = {
-		load_on_startup = true,
+  nvdash = {
+    load_on_startup = true,
     buttons = {
       { "  Find File", "Spc f f", "Telescope find_files" },
       { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
@@ -47,12 +47,12 @@ M.ui = {
       -- { "  Themes", "Spc t h", "Telescope themes" },
       -- { "  Mappings", "Spc c h", "NvCheatsheet" },
     },
-	},
+  },
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require("custom.mappings")
+M.mappings = require "custom.mappings"
 
 return M
