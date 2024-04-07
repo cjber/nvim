1ddfa21005d8b6d014076c714564a5b89093d50c
cjber <cjberragan@gmail.com>
2024-04-07 17:44:18 +0100

    add docs to cmp

--- configs/extras/cmp.lua
+++ configs/extras/cmp.lua
@@ -1,5 +1,6 @@
 local spec = {
   "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-nvim-lsp-signature-help" },
   opts = function()
     local opts = require "plugins.configs.cmp"
     local cmp = require "cmp"
@@ -9,12 +10,13 @@ local spec = {
     }
 
     opts.mapping["<CR>"] = cmp.mapping.confirm { select = false }
-    opts.view = { docs = { auto_open = false } }
+    opts.view = { docs = { auto_open = true } }
 
     opts.sources = {
-      { name = "luasnip" },
       { name = "nvim_lsp" },
+      { name = "nvim_lsp_signature_help" },
       { name = "nvim_lua" },
+      { name = "luasnip" },
       {
         name = "path",
         option = {
